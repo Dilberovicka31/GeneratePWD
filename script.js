677c2401d8bbcefcc05c72555bb8d3c3d102713d
Dilberovicka31 <dilberovicm36@gmail.com>
2020-10-25 16:16:43 -0700

    changed the variables

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ function writePassword() {
 
 
 // Character set to choose the password from
-var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
-var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var numericPs = '0123456789';
-var specialChar = '!\"#$%&';
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericPs = ["0","1","2","3","4","5","6","7","8","9"];
+var specialChar = ["!","\\","#","$","%","&","'","(",")","*","+","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~","\""];
 var allChar = "";
 
 console.log(lowerCase,upperCase,numericPs,specialChar);
@@ -36,9 +36,7 @@ console.log(lengthReq,specialCharReq,numericReq,lowerCs,upperCs);
 function generatePassword() {
 
 
-if (allChar <8 || allChar >128 ) {
-  alert ("Password length should be between 8 and 128")}
-console.log (allChar);
+
 
 
 }
@@ -50,6 +48,8 @@ console.log (allChar);
   //If using special characters then push the characters into chartoUse
 
   //If using number characters then push the characters into chartoUse
+
+  // 
 
 
   while(password.length < passwordLength) {}
